a0534db74fc819f05bb92edd26bba9fe84181788
RutvikTak <65209850+rutvik110@users.noreply.github.com>
2022-01-02 19:11:41 +0530

    reverted changes

--- android/src/main/java/com/code/aaron/micstream/MicStreamPlugin.java
+++ android/src/main/java/com/code/aaron/micstream/MicStreamPlugin.java
@@ -59,13 +59,13 @@ public class MicStreamPlugin implements FlutterPlugin, EventChannel.StreamHandle
     // Audio recorder + initial values
     private static volatile AudioRecord recorder;
 
-    private int AUDIO_SOURCE = MediaRecorder.AudioSource.VOICE_RECOGNITION;
+    private int AUDIO_SOURCE = MediaRecorder.AudioSource.DEFAULT;
     private int SAMPLE_RATE = 16000;
     private int actualSampleRate;
     private int CHANNEL_CONFIG = AudioFormat.CHANNEL_IN_MONO;
     private int AUDIO_FORMAT = AudioFormat.ENCODING_PCM_8BIT;
     private int actualBitDepth;
-    private int BUFFER_SIZE =  256;//AudioRecord.getMinBufferSize(SAMPLE_RATE, CHANNEL_CONFIG, AUDIO_FORMAT);
+    private int BUFFER_SIZE = AudioRecord.getMinBufferSize(SAMPLE_RATE, CHANNEL_CONFIG, AUDIO_FORMAT);
 
     // Runnable management
     private volatile boolean record = false;
@@ -181,7 +181,7 @@ public class MicStreamPlugin implements FlutterPlugin, EventChannel.StreamHandle
                 AUDIO_SOURCE = config.get(0);
             default:
                 try {
-                    BUFFER_SIZE = 256; // AudioRecord.getMinBufferSize(SAMPLE_RATE, CHANNEL_CONFIG, AUDIO_FORMAT);
+                    BUFFER_SIZE = AudioRecord.getMinBufferSize(SAMPLE_RATE, CHANNEL_CONFIG, AUDIO_FORMAT);
                 } catch (Exception e) {
                     eventSink.error("-3", "Invalid AudioRecord parameters", e);
                 }
